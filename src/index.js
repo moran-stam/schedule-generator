72ccd89d28d3f7ee2d471f14ce7d1f787a4ed378
moran-stam <moran.stam.email@gmail.com>
2025-03-19 20:08:27 +0200

    Add src/index.js

---
+++ src/index.js
@@ -0,0 +1,20 @@
+// Main entry point for the application
+
+function initApp() {
+  console.log('Schedule Generator initialized');
+  // Application initialization code will go here
+}
+
+function generateSchedule(config) {
+  // Schedule generation algorithm will be implemented here
+  return {
+    success: true,
+    schedule: [],
+    message: 'Schedule generation not yet implemented'
+  };
+}
+
+module.exports = {
+  initApp,
+  generateSchedule
+};
